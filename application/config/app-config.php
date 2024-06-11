104cfa72322ef37e333644f890b76cd314f041d8
rifat977 <itscrifat5147@gmail.com>
2024-06-12 02:14:28 +0600

    base url

--- application/config/app-config.php
+++ application/config/app-config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 * environments.
 *
 */
-define('APP_BASE_URL', 'http://161.97.146.24/1xbaji/');
+define('APP_BASE_URL', 'http://161.97.146.24/1xbaji/index.php/');
 
 /*
 * --------------------------------------------------------------------------
